614582703f5655f9c4130969a64574dee9352b75
Ekaterina-987 <77798912+Ekaterina-987@users.noreply.github.com>
2022-08-23 22:47:08 +0300

    Домашнее задание 4

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+const input = document.querySelector('#input');
+const items = document.querySelector('#items');
+input.addEventListener('keydown', function(event) {
+    const text = input.value;
+
+    const newItems = document.createElement('div');
+    newItems.classList.add('input');
+    newItems.textContent = text;
+
+    newItems.addEventListener('click', function() {
+        newItems.classList.toggle('done');
+    });
+    if (event.keyCode == 13) {
+        items.append(newItems);
+        input.value = '';
+    }
+});
+
+const allItems = document.querySelectorAll('#items');
+for (let addDeleteDone of i) {
+    addDeleteDone[allItems].addEventListener('click', function() {
+        addDeleteDone[allItems].classList.toggle('done');
+    })
+
+}
 
 /* Пустые элементы не должны добавляться */
 
